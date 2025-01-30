7b07ea22d970f84a07d3c70346c5e118ee1d1179
DamianT325 <dtramowski7899@gmail.com>
2025-01-30 21:58:11 +0100

    changesdsads

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import csv
+import os
 # Main data including income, expense and total stored in dict
-data = {'income': [], 'expense': [],'total': 0 }
+data = {'income': [], 'expense': [],'total': [] }
+# Filename to save csv and also to check if already
+filename = "dane.csv"
+file_exist = os.path.isfile(filename)
+
+def read_file(filename="dane.csv"):
+    if file_exist:
+        with open(filename, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file, delimiter=";")
+            next(csv_reader)
+            return list(csv_reader)
+    else: 
+        print("Brak pliku stwórz najpierw plik")
+
+def update_file_total(filename="dane.csv", total=0):
+    if file_exist:
+        with open(filename, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file, delimiter=";")
+            next(csv_reader)
+            total_row = next(csv_reader)
+            total_cell = total_row[2]
+            total_cell = total
+
+    else: 
+        print("Brak pliku stwórz najpierw plik")
+
 
 # Adding income to main data dict
 def add_income():
@@ -28,12 +55,41 @@ def add_expense():
 
 
 def update_total(data):
-    data['total'] = sum(data['income']) - sum(data['expense'])
+    if file_exist:
+        data = read_file()
+        update_file_total(2000)
+        print(data[0][2])
+    # data['total'].append(sum(data['income']) - sum(data['expense']))
+  
+        
     
 def show_total():
-    print(f'{data["total"]:.2f}zł')
+    # print(f'{data["total"][0]:.2f}zł')
+        if file_exist:
+            total = read_file()
+            print(f"Saldo (income-expsense): {total}")
+        else: 
+            print("Brak pliku stwórz najpierw plik")
 
-add_income()
-add_expense()
+def save_to_csv():
+    with open(filename, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        # check if file already exist and not adding headers
+        if not file_exist:
+            # Nagłowki 
+            writer.writerow(['income', 'expense', 'total'])
+
+        # for income, expense, total in zip(data['income'], data['expense'], data['total']):
+        #     writer.writerow([income, expense, total])
+        for i in range(len(data['income'])):
+                writer.writerow([data['income'][i], data['expense'][i]])
+    
+    # print(f"Dane zostały poprawnie zapisane do pliku {filename}")
+
+# TODO CSV handler dla write i read
+
+# add_income()
+# add_expense()
 update_total(data)
-show_total()
+# show_total()
+# save_to_csv()
